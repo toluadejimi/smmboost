5d7938a14db447a06e9c013f0b476874a65c3baa
toluadejimi <toluadejimi@gmail.com>
2025-09-24 21:41:25 +0100

    jhgfg

--- resources/views/themes/osocialcare/user/fund/add_fund.blade.php
+++ resources/views/themes/osocialcare/user/fund/add_fund.blade.php
@@ -90,11 +90,11 @@
                                 required="">
                             <div class="error-message" id="amount-error"></div>
                         </div>
-                        <div style="display: none" id="currency-section">
+                        <div  id="currency-section">
                             <label class="text-white text-sm">Supported Currency</label>
                             <select name="supported_currency" id="supported_currency"
                                 class="w-full rounded-lg bg-gray-700 text-gray-100 border border-gray-600 focus:ring-2 focus:ring-orange-500 focus:border-orange-500 outline-none p-3 text-xs transition-all duration-200">
-                                <option value="" >NGN</option>
+                                <option value="NGN">NGN</option>
                             </select>
                         </div>
                         <div class="hidden" id="crypto-currency-section">
